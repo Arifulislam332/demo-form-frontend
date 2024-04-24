e185c02753c130035e94471a69877d0372dbabee
Arifulislam332 <ayatullaharif332@gmail.com>
2024-04-24 18:40:20 +0600

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,22 @@
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-20 py-10 bg-pink-500">
+      <div className="flex flex-col md:flex-row justify-between text-white items-center container mx-auto">
+        <Link href="/" className="font-bold text-2xl">
+          Home
+        </Link>
+
+        <div className="flex gap-5 items-center">
+          <Link href="/">Privacy Policy</Link>
+          <Link href="/">FAQs</Link>
+          <Link href="/">Terms of Service</Link>
+          <Link href="mailto:dish@dash09.com">Contact Us</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
