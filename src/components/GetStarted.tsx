10f6a6cd0c6e9c7f5865e9d665f9b1760d37d267
Arifulislam332 <ayatullaharif332@gmail.com>
2024-04-25 22:44:52 +0600

    get started

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -1,5 +1,20 @@
 const GetStarted = () => {
-  return <div id="get__started">GetStarted</div>;
+  return (
+    <div
+      id="get__started"
+      className="mt-20 flex flex-col container mx-auto gap-20"
+    >
+      <div className="flex flex-col gap-5 text-center py-10 rounded-xl border shadow-md">
+        <h2 className="text-pink-500 text-2xl md:text-4xl font-bold">
+          Lorem ipsum dolor sit amet.
+        </h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore,
+          pariatur
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default GetStarted;
